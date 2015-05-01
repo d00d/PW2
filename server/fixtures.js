d4d8ac3a9396d7cc0d4bb53743f3e2a1556cba1e
rrd- <rdubnick@gmail.com>
2015-04-30 23:48:37 -0700

    PW2 - Added stocks route and loading template

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,16 @@
 if (Stocks.find().count() === 0) {
 	Stocks.insert({
-		ticker: 'AAPL',
+		ticker: 'ELNK',
 		url: 'https://www.youtube.com/watch?v=Kve1jq0psh8'
 	});
 
 	Stocks.insert({
-		ticker: 'BMO',
+		ticker: 'NGLS',
 		url: 'https://www.youtube.com/watch?v=Kve1jq0psh8'
 	});
 	
 	Stocks.insert({
-		ticker: 'NGLS',
+		ticker: 'BMO',
 		url: 'https://www.youtube.com/watch?v=Kve1jq0psh8'
 	});
 	
